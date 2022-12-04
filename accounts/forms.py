625fa4af1c6381e91479218f0296d96237e372ae
ArManiyar <arsufiyan.maniyar@gmail.com>
2022-12-04 16:19:00 +0530

    vendor profile and vendor dashboard

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from .models import User
-
+from .models import User, UserProfile
+from accounts.validators import allow_only_images_validator
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
@@ -19,4 +19,19 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'phone_number']
 
-    
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'start typing....', 'required':'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city','pin_code', 'longitutde', 'latitude']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitutde':
+                self.fields[field].widget.attrs['readonly']='readonly'
+                
+    # longitutde = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'})) #TODO 1st way to readonly
--- vendor/urls.py
+++ vendor/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from accounts import views as accountsviews
 
+
 urlpatterns = [
     path('', accountsviews.vendorDashboard, name='vdashboard'),
     path('profile/', views.vprofile, name='vprofile'),
